b9eb11cd9412a0a6b18821256256f740e3902729
Ronny Orbach <r@ronnyo.com>
2012-03-29 17:56:43 +0200

    Refactor, Cache, Actually replace stylesheets

--- jquery.cssp.js
+++ jquery.cssp.js
@@ -1,22 +1,43 @@
-jQuery.fn.cssP = function(changes){
-	var $this = $(this);
-	var s = $this.selector;
-	var stylesheet = '';
-	$.each(changes, function(key, ruleset){
-		var newRuleset,
-			declaration;
-		
-		if(typeof ruleset === 'function') {
-			newRuleset = ruleset();
-		} else if(typeof ruleset === 'string') {
-			newRuleset = ruleset;
+(function($){
+
+$.cssP = (function(){
+	var cache = {};
+	var methods = {};
+	$.extend(methods, {
+		updateEl: function(el, changes){
+			var s = el.selector;
+			var styleTag = cache[s] || $('<style></style>').appendTo('body');
+			cache[s] = styleTag;
+			
+			var stylesheet = '';
+			$.each(changes, function(key, ruleset){
+				var newRuleset,
+					declaration;
+				
+				if(typeof ruleset === 'function') {
+					newRuleset = ruleset();
+				} else if(typeof ruleset === 'string') {
+					newRuleset = ruleset;
+				}
+				
+				declaration = s + ':' + key + ' {' + newRuleset + '}';
+				stylesheet += declaration + '\r';
+			});
+			
+			$(el).trigger('update.cssp', {"tag": styleTag, "sheet": stylesheet});
+		},
+		writeStylesheet: function(ev, d){			
+			$(d.tag).text(d.sheet);
 		}
-		
-		declaration = s + ':' + key + ' {' + newRuleset + '}';
-		stylesheet += declaration + '\r';
 	});
-	
-	console.log(stylesheet);
-	
-	return $(this);
-};
+
+	$.fn.cssP = function(changes){
+		var $this = $(this);
+		$this.bind('update.cssp', methods.writeStylesheet);
+		methods.updateEl($this, changes);
+		return $this;
+	};
+})();
+
+
+})(jQuery);
