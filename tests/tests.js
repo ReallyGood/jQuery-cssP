acf96815c66b2de69387d3ae35ebb75cfe7af695
Ronny Orbach <r@ronnyo.com>
2012-04-12 11:46:53 +0300

    test modules

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,13 @@
 function startTests(){
 
+module('Basics');
+
 test('Simple Color', function() { 
-    equal( $('h1').css('color'), 'rgb(0, 0, 0)');
+    equal( $('#basics h1').css('color'), 'rgb(0, 0, 0)');
 })
 
 test('basic functionality', function(){
-	ok( $('h1').length > 0, 'Hello World');
+	ok( $('#basics h1').length > 0, 'Hello World');
 });
 
 }
